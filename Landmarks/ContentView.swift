7135889f04685be6408aa97a02c942fa17dd092f
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-05 15:11:14 +0900

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by INOUE Takuya on 2020/02/05.
+//  Copyright © 2020 INOUE Takuya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
