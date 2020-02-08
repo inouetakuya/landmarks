881f717325dd5fc524e1912469ef5d75e40399f8
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-08 22:15:02 +0900

    MapKit で地図を表示させた

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by INOUE Takuya on 2020/02/08.
+//  Copyright © 2020 INOUE Takuya. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 35.711308, longitude: 139.796355)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
