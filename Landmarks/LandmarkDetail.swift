1b50b6b0edf554de2d1b2678cd07b342c0fc23fc
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-12 00:07:00 +0900

    Section 7 Pass Data into Child Views

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -10,38 +10,41 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
             
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text("雷門")
+                Text(landmark.name)
                     .font(.title)
                     .fontWeight(.bold)
                     .foregroundColor(Color.black)
                     .padding(.bottom, 8)
                 HStack {
-                    Text("浅草寺")
+                    Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text("東京都")
+                    Text(landmark.state)
                         .font(.subheadline)
                 }
             }.padding()
 
             Spacer()
         }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
